95a3b87a5f5e1ae05e5678588210b930470509e7
Moisito12 <moismm1996@outlook.com>
2020-07-08 13:28:04 -0500

    Addind jwt for tokens

---
+++ app/Helpers/JwtAuth.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helpers;
+
+use Firebase\JWT\JWT;
+use Illuminate\Support\Facades\DB;
+use App\User;
+
+class JWTAuth
+{
+    public function signup()
+    {
+        // Buscar si existe el usuario en la bd
+
+        // Comprobar si las credenciales son correctas
+
+        // Generar el token con los datos del usuario identificado
+
+        // Devolver los datos decoificados o el token, en función de un parámetro
+
+        return "Método de la clase JWT";
+    }
+}
